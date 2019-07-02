19bb24aaac99f1cb97434f4c815e506407389bab
Hannah Cole <hannahcole@fb.com>
2019-07-02 16:25:59 -0700

    chars left

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,14 +3,18 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ComposeActivity extends AppCompatActivity {
 
     Button btnTweet;
     EditText tvTweetInput;
+    TextView tvCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         btnTweet = (Button) findViewById(R.id.btnTweet);
         tvTweetInput = (EditText) findViewById(R.id.tvTweetInput);
+        tvCounter = (TextView) findViewById(R.id.tvCounter);
 
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +33,25 @@ public class ComposeActivity extends AppCompatActivity {
                 data.putExtra("tweet", tvTweetInput.getText().toString());
                 setResult(RESULT_OK, data); // set result code and bundle data for response
                 finish();
+            }
+        });
+
+        tvTweetInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String charsLeft = Integer.toString(240 - s.length());
+                tvCounter.setText(charsLeft + " chars left");
+
             }
         });
     }
